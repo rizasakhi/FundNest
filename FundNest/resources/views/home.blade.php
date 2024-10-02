018f1c62dd8b3c03cf3cd0564a8c4dc0989e83d0
Riza Sakhi <rizasakhi3@gmail.com>
2024-10-02 19:39:31 +0700

    mengubah route dan menambah file home

---
+++ FundNest/resources/views/home.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>FundNest</title>
+</head>
+<body>
+    {{-- Navbar --}}
+    <nav class="navbar navbar-expand-lg navbar-light bg-light container-fluid col-8">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <h1>LOGO</h1>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">link</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+    {{-- Heroes --}}
+    <div class="container col-xxl-8 px-4 py-5" bis_skin_checked="1">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5" bis_skin_checked="1">
+            <div class="col-10 col-sm-8 col-lg-6" bis_skin_checked="1">
+                <img src="https://img.freepik.com/free-vector/crowdfunding-flat-cartoon-concept-with-human-hands-throwing-coins-productive-idea-growth-symbols-vector-illustration_1284-84401.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6" bis_skin_checked="1">
+                <h1 class="display-5 fw-bold lh-1 mb-3">Lorem ipsum dolor sit amet consectetur.</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas sed esse ex temporibus ad repellendus iusto adipisci quisquam similique ullam! Similique expedita fuga voluptatum nulla? Sit eius laborum laudantium excepturi tempore, nobis nemo.</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start" bis_skin_checked="1">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">lorem</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">lorem</button>
+            </div>
+        </div>
+    </div>
+
+    {{-- Footer --}}
+    <div class="container" bis_skin_checked="1">
+        <footer class="py-5">
+          <div class="row" bis_skin_checked="1">
+            <div class="col-6 col-md-2 mb-3" bis_skin_checked="1">
+              <h5>Section</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+              </ul>
+            </div>
+      
+            <div class="col-6 col-md-2 mb-3" bis_skin_checked="1">
+              <h5>Section</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+              </ul>
+            </div>
+      
+            <div class="col-6 col-md-2 mb-3" bis_skin_checked="1">
+              <h5>Section</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+              </ul>
+            </div>
+      
+            <div class="col-md-5 offset-md-1 mb-3" bis_skin_checked="1">
+              <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of what's new and exciting from us.</p>
+                <div class="d-flex flex-column flex-sm-row w-100 gap-2" bis_skin_checked="1">
+                  <label for="newsletter1" class="visually-hidden">Email address</label>
+                  <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                  <button class="btn btn-primary" type="button">Subscribe</button>
+                </div>
+              </form>
+            </div>
+          </div>
+      
+          <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top" bis_skin_checked="1">
+            <p>© 2024 Company, Inc. All rights reserved.</p>
+            <ul class="list-unstyled d-flex">
+              <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+              <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+              <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
